76ce588decad115c1ff0f5ce4dfefe91a40acea3
Hassan Abbas <hassanabbascontact@gmail.com>
2023-03-16 15:00:56 +0200

    Guest Layout, Signup + login

--- client/src/contexts/ContextProvider.jsx
+++ client/src/contexts/ContextProvider.jsx
@@ -13,8 +13,7 @@ export const ContextProvider = ({children}) => {
     const [user, setUser] = useState({
         name: "Hassan"
     });
-    const [token, _setToken] = useState('123');
-    //localStorage.getItem('ACCESS_TOKEN')
+    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
 
     const setToken = (token) => {
         _setToken(token)
