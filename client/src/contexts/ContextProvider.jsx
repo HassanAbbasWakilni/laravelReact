0e3cdcea5b0db5d31d4ec7edb34b8b901896007e
Hassan Abbas <hassanabbascontact@gmail.com>
2023-03-16 14:16:53 +0200

    Default Layout and design

--- client/src/contexts/ContextProvider.jsx
+++ client/src/contexts/ContextProvider.jsx
@@ -10,8 +10,11 @@ const StateContext = createContext({
 
 export const ContextProvider = ({children}) => {
 
-    const [user, setUser] = useState({});
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const [user, setUser] = useState({
+        name: "Hassan"
+    });
+    const [token, _setToken] = useState('123');
+    //localStorage.getItem('ACCESS_TOKEN')
 
     const setToken = (token) => {
         _setToken(token)
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ContextProvider } from './contexts/ContextProvider';
 import router from './Router';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
