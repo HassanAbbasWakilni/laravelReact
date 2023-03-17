7033e7eac1e46a215996e2791f2430863c69db3e
Hassan Abbas <hassanabbascontact@gmail.com>
2023-03-17 11:25:48 +0200

    Login Done

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -11,25 +11,23 @@ const Signup = () => {
     const passwordConfirmationRef = useRef();
 
     const [errors, setErrors] = useState(null);
-
     const {setUser, setToken} = useStateContext();
 
     const handleSignup = (e) => {
         e.preventDefault()
-        const newUser = {
+        const payload = {
             name: nameRef.current.value,
             email: emailRef.current.value,
             password: passwordRef.current.value,
             password_confirmation: passwordConfirmationRef.current.value,
         }
-        axiosClient.post('/signup', newUser)
+        axiosClient.post('/signup', payload)
             .then(({data}) => {
                 setUser(data.user);
                 setToken(data.token);
             }).catch(err => {
                 const response = err.response;
                 if(response && response.status === 422){
-                    console.log(response)
                     setErrors(response.data.errors);
                 }
             })
